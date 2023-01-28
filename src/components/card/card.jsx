f340f5c937b8a61c313e2fecbc7d820533d34b2a
Tigran <tapetrosian@gmail.com>
2023-01-28 13:45:28 +0200

    add text-container to card removed extra paddings

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -5,8 +5,10 @@ const Card = ({ dogInfo, path }) => {
     return (
         <div className={path !== '' ? style.card : style['carousel-card']}>
             <img src={dogInfo.image.url} alt="dog" className={style.image} />
-            <h3 className={style.name}>{dogInfo.name}</h3>
-            <p className={style.description}>{dogInfo.bred_for}</p>
+            <div className={style['text-container']}>
+                <h3 className={style.name}>{dogInfo.name}</h3>
+                <p className={style.description}>{dogInfo.bred_for}</p>
+            </div>
         </div>
 
     );
